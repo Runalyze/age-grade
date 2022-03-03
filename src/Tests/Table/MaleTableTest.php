08243a9dbe968b7295bd85b5f26fc388ff690483
laufhannes <mail@laufhannes.de>
2022-03-03 10:34:29 +0100

    update tables to v2020

--- src/Tests/Table/MaleTableTest.php
+++ src/Tests/Table/MaleTableTest.php
@@ -25,17 +25,17 @@ class MaleTableTest extends \PHPUnit\Framework\TestCase
 
     public function testSomeValues()
     {
-        $this->assertEquals(0.8312, round($this->Table->getAgePerformance(15, 5.0, 16 * 60 + 10), 4));
-        $this->assertEquals(0.7633, round($this->Table->getAgePerformance(27, 10.0, 35 * 60 + 0), 4));
-        $this->assertEquals(0.8555, round($this->Table->getAgePerformance(42, 42.2, 2 * 60 * 60 + 29 * 60 + 59), 4));
-        $this->assertEquals(0.7329, round($this->Table->getAgePerformance(54, 10.0, 42 * 60 + 25), 4));
+        $this->assertEquals(0.8227, round($this->Table->getAgePerformance(15, 5.0, 16 * 60 + 10), 4));
+        $this->assertEquals(0.7543, round($this->Table->getAgePerformance(27, 10.0, 35 * 60 + 0), 4));
+        $this->assertEquals(0.8499, round($this->Table->getAgePerformance(42, 42.2, 2 * 60 * 60 + 29 * 60 + 59), 4));
+        $this->assertEquals(0.7269, round($this->Table->getAgePerformance(54, 10.0, 42 * 60 + 25), 4));
     }
 
     public function testSomeOpenStandardValues()
     {
         $this->assertEquals(6.39, $this->Table->getAgeStandard(25, 0.06));
         $this->assertEquals(440.67, $this->Table->getAgeStandard(25, 3.0));
-        $this->assertEquals(7377.0, $this->Table->getAgeStandard(25, 42.195));
+        $this->assertEquals(7299.0, $this->Table->getAgeStandard(25, 42.195));
         $this->assertEquals(52800.0, $this->Table->getAgeStandard(25, 200.0));
     }
 
